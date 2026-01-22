bb39cf347114313137ce51634995294f82f01e15
MICHELLE LIPENGA <166696321+ShelbyMichelle@users.noreply.github.com>
2026-01-22 12:05:36 -0800

    added online postgres url

--- database.js
+++ database.js
@@ -1,17 +1,19 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,      // database name
-  process.env.DB_USER,      // username
-  process.env.DB_PASSWORD,  // password
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // add port for PostgreSQL
-    dialect: 'postgres',      // changed from mysql to postgres
-    logging: false
-  }
-);
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
